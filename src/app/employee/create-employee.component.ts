5950793c3b08a9e9991ebff12bab70a803fcb4d7
Irinej Stjepanovic <irinejs@gmail.com>
2019-12-26 17:06:01 +0100

    Moving validation from UI to components

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -11,6 +11,7 @@ export class CreateEmployeeComponent implements OnInit {
   formErrors = {
     'fullName': '',
     'email': '',
+    'phone': '',
     'skillName': '',
     'experienceInYears': '',
     'proficiency': ''
@@ -23,6 +24,9 @@ export class CreateEmployeeComponent implements OnInit {
     },
     'email': {
       'required': 'Email is required.'
+    },
+    'phone' : {
+      'required' : 'Phone is required.'
     },
     'skillName': {
       'required': 'Skill Name is required.',
@@ -40,23 +44,28 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      eMail: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
         skills: this.fb.group({
           skillName: ['', Validators.required],
           experienceInYears: ['', Validators.required],
           proficiency: ['', Validators.required]
         })
     });
+    this.employeeForm.valueChanges.subscribe((data) => {
+      console.log(this.logValidationErrors(this.employeeForm));
+    })
   }
 
-  logValidationErrors(group: FormGroup) : void {
+  logValidationErrors(group: FormGroup = this.employeeForm) : void {
     Object.keys(group.controls).forEach((key: string) =>{
       const abstractControl = group.get(key);
       if(abstractControl instanceof FormGroup){
         this.logValidationErrors(abstractControl);
       } else {
         this.formErrors[key] = '';
-        if(abstractControl && !abstractControl.valid) {
+        if(abstractControl && !abstractControl.valid && 
+          (abstractControl.touched || abstractControl.dirty)) {
           const messages = this.validationMessages[key];
           
           for ( const errorKey in abstractControl.errors){
@@ -70,8 +79,8 @@ export class CreateEmployeeComponent implements OnInit {
   }
 
   onLoadDataClick(): void {
-    this.logValidationErrors(this.employeeForm);
-    console.log(this.formErrors);
+   // this.logValidationErrors(this.employeeForm);
+   // console.log(this.formErrors);
   }
 
   onSubmit(): void {
