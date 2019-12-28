8cc717e935c9958672e2557e1ce90699f5a4c490
Irinej Stjepanovic <irinejs@gmail.com>
2019-12-28 16:40:05 +0100

    Custom validator with parameter

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -107,12 +107,14 @@ export class CreateEmployeeComponent implements OnInit {
   }
 }
 
-function emailDomain(control: AbstractControl): { [key: string]: any } | null {
-  const email: string = control.value;
-  const domain = email.substring(email.lastIndexOf('@') + 1);
-  if (email === '' || domain.toLowerCase() === 't-com.hr') {
-    return null;
-  } else {
-    return { 'emailDomain': true };
-  }
+function emailDomain(domainName: string) {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const email: string = control.value;
+    const domain = email.substring(email.lastIndexOf('@') + 1);
+    if (email === '' || domain.toLowerCase() === domainName.toLowerCase()) {
+      return null;
+    } else {
+      return { 'emailDomain': true };
+    }
+  };
 }
